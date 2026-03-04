352b53c95677f1339f01e8425ac14baf6a5f26d2
Roberto4-99 <robertosinclair.fn@gmail.com>
2026-03-04 21:03:26 +0100

    add monetag ads

--- public/ads.js
+++ public/ads.js
@@ -1,5 +1,5 @@
 var s = document.createElement("script");
 s.src = "https://5gvci.com/act/files/tag.min.js?z=10683065";
-s.setAttribute("data-cfasync","false");
 s.async = true;
+s.setAttribute("data-cfasync","false");
 document.body.appendChild(s);
